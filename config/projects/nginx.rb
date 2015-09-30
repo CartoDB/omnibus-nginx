a2a4fe4dda63e111654d027112536d59c4727808
Hugo Sato <hsato42@users.noreply.github.com>
2015-09-30 17:06:20 -0400

    Update nginx.rb

--- config/projects/nginx.rb
+++ config/projects/nginx.rb
@@ -1,10 +1,6 @@
-#
-# This project is a container of common libraries used by applications in CartoDB stack
-# Software dependents of these libraries shouldn't need to rebuild them but just deploy the produced package
-#
 name "nginx"
-maintainer "Data Visualization WEBMAPS team"
-homepage "https://cms.prod.bloomberg.com/team/display/datavis/Webmap"
+maintainer "CartoDB team"
+homepage "https://github.com/CartoDB/omnibus-nginx/blob/master/README.md"
 
 build_version Omnibus::BuildVersion.semver
 build_iteration 1
