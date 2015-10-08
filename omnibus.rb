a472680db5d9da2752e2efb7592e53458a1ee11e
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-08 19:57:44 -0400

    Update omnibus.rb

--- omnibus.rb
+++ omnibus.rb
@@ -1,3 +1,5 @@
 base_dir ENV['OMNIBUS_BASE_DIR'] || ( ENV['LOGNAME'] == 'jenkins' ? '/var/lib/jenkins/userContent/omnibus' : './local/omnibus' )
 
 software_gems ['omnibus-software']
+
+build_retries 3
